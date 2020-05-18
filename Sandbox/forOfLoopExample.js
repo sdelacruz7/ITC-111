9d3bb973d4a779ad5e13690bd7a336fd5dcfa4a0
sdelacruz7 <sdelacruz@sea.yearup.org>
2020-05-18 14:01:56 -0700

    updated new files

---
+++ Sandbox/forOfLoopExample.js
@@ -0,0 +1,11 @@
+//ForOf Loop - used to iterate/loop through elements in an array
+
+//syntax: for(elemtent of array) {do stuff;}
+
+
+var fruits = ['apple','orange','pear'];
+var e;
+
+for(e of fruits){
+    console.log(fruits[e]);
+}
