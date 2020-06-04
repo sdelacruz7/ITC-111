9dd4ff7ae437f17b86756a955f8fce45cdca3e00
sdelacruz7 <sdelacruz@sea.yearup.org>
2020-06-04 12:33:55 -0700

    COMMIT

--- Class Exercises/scripts.js
+++ Class Exercises/scripts.js
@@ -3,13 +3,13 @@ function loadDisplay(num) {
     document.getElementById('display2').value += num.value.toString();
 }
 function delFromRight() {
-    document.getElementById('display2').value = document.getElementById('display2').value.slice(0, -1);
+    document.getElementById('display2').value = document.getElementById('display2').value.slice(0,-1);
 }
 function clearDisplay() {
     document.getElementById('display2').value = "";
 }
 function setOp(tag) {
-    val1 = document.getElementById('display2').value + " " + tag.value;
+    val1 = document.getElementById('display2').value + " " + trg.value;
     document.getElementById('display1').innerHTML = val1;
     document.getElementById('display2').value = "";
 
